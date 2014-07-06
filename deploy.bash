a662ea7481b5498800c9a1dfeadcc448b77d3e49
Hanno Rein <hanno@hanno-rein.de>
2014-07-06 03:43:20 +0200

    removed deploy script

--- deploy.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git push
-ssh hanno-rein.de <<'ENDSSH'
-cd /home/hanno/public_html/exoplanet.hanno-rein.de/oec_meta
-echo "Starting update on"
-hostname
-git pull
-echo "Update done."
-ENDSSH
